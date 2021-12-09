0ff38d9af7ec4e3242ba6be77890c8e07ff641f2
hoilamc <lilychan1680806@gmail.com>
2021-12-09 23:35:01 +0000

    clean code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mancala
+A simple game play of Mancala. You can play with the code.
+
+## How to play
+1. Make sure you have python3 installed.
+   My version: `3.8.10`
+1. Run the code.
+   ```
+   python main.py
+   ```
+1. Enjoy!
+
+## What am I doing?
+I aim to practice and learn the following:
+* Design patterns
+* Clean code
+* Machine Learning
--- main.py
+++ main.py
@@ -4,23 +4,52 @@ from lib.game import Game
 from lib.view.playboard_renderer import PlayboardRenderer
 
 import time
+import textwrap
+import math
 
 START_PLAYER = "B"
+GREETINGS = """
+~~~~~~~~~~~~~~~~~~~~~HELLO~~~~~~~~~~~~~~~~~~~~~
+Greetings! Thank you for playing with me.
+I will be player A, and you will be player B.
+
+How to play?
+There will be a board with numbers indicating
+the number of stones in the cell. Like this:
+
+{}
+
+Choose a cell by typing "a", "s" or "d" to
+move the stones inside. Each of them will go to
+the preceding cells accordingly.
+
+The one who collects the most number of stones
+in the goal wins.
+
+You can press enter to start now!
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 
 if __name__ == "__main__":
     game = Game()
     playboard = PlayboardRenderer(game.PLAYERS.values())
     players = list(game.PLAYERS.keys())
-    print(f"Players: {players}")
+
+    input(GREETINGS.format(playboard.PLAYBOARD))
 
     current_player = START_PLAYER
     turn = 0
 
     while True:
+        print(textwrap.dedent(f"""\
+        ======================================
+        turn [{math.floor(turn/2)}]: Player {current_player}
+        ======================================
+        """))
         print(playboard.render())
-        print(f"turn [{turn}]: Player {current_player}")
         game.PLAYERS[current_player].play()
         game.judge()
         turn += 1
-        if current_player != "B": time.sleep(1)
+        if current_player != "B":
+            time.sleep(1)
         current_player = players[turn % len(players)]
